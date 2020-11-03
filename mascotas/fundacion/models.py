0cb43036755483e31d953efba25603052d96d5c5
iPoe <leonardosawi12@gmail.com>
2020-11-03 11:09:25 -0500

    agregar modelo de mascota

--- mascotas/fundacion/models.py
+++ mascotas/fundacion/models.py
@@ -16,6 +16,20 @@ class UsuarioFundacion(AbstractBaseUser):
 	USERNAME_FIELD = 'correo'
 	REQUIRED_FIELDS = ['nombre_fund']
 
+class mascota(models.Model):
+	Nombre = models.CharField(max_length=20)
+	Descripcion = models.CharField(max_length=255)
+	opciones_tipo = models.TextChoices('Tipo','Perro Gato Otro')
+	Tipo_Mascota = models.CharField(choices=opciones_tipo.choices,max_length=10)
+	Edad = models.IntegerField()
+	opciones_genero = (
+			('F','Femenino'),
+			('M','Masculino')
+		)
+	genero = models.CharField(max_length=1,choices=opciones_genero)
+	Estado_esterilzacion = models.CharField(max_length=255)
+	Estado_salud = models.CharField(max_length=255)
+
 	#objects = MyAccountManager()
 
 # Create your models here.
