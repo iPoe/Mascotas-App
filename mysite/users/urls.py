d407943ab042d94872ee60c251f9c9d128c53dfe
iPoe <leonardosawi12@gmail.com>
2020-11-21 11:42:20 -0500

    usar loginview

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from users.views import (adoptantes,fundacion)
+from django.contrib.auth import views as auth_views
+
 
 app_name = 'users'
 
@@ -7,7 +9,8 @@ urlpatterns = [
 	path('reg/', adoptantes.AdoptSignUpView.as_view(),name="registro"),
 	path('regf/',fundacion.FundtSignUpView.as_view(),name="registroFundacion"),
 	#path('log/',adoptantes.UserLoginView.as_view(),name="login"),
-	path('login/',adoptantes.LoginView.as_view(),name="login"),	
+	#path('login/',adoptantes.LoginView.as_view(),name="login"),
+	path('l/',auth_views.LoginView.as_view(),{'template_name':'adoptantes/login_1.html'},name="login"),
 	path('main/',adoptantes.vista_main,name="main")
 	
 ]
